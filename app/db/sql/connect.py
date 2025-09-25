b87fe29f81e3a82efbeac633aac25eaf037f8cf8
JulianKropp <62835694+JulianKropp@users.noreply.github.com>
2025-09-25 15:36:41 +0200

    🐛 Fix docker

--- app/db/sql/connect.py
+++ app/db/sql/connect.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./feuerwehr.db"
+DATABASE_URL = "sqlite+aiosqlite:///./data/feuerwehr.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
